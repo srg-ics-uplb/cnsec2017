63b5e684e2a4da6faae507c8c86232c0e88c6b88
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2017-06-12 19:46:38 +0800

    fixed directories for dvwa

--- setup-env.sh
+++ setup-env.sh
@@ -22,10 +22,11 @@ apt-get install -y build-essential nasm
 wget https://github.com/ethicalhack3r/DVWA/archive/v1.9.tar.gz -O /tmp/v1.9.tar.gz
 cd /var/www/html
 tar xzvf /tmp/v1.9.tar.gz
-mv DVWA-1.9 dvwa
+cp -r DVWA-1.9/* .
+rm -fr DVWA-1.9
 sed -i 's/allow_url_include = Off/allow_url_include = On/g' /etc/php/5.6/apache2/php.ini
-chmod -R 777 /var/www/html/dvwa/hackable/uploads/
-chmod -R 777 /var/www/html/dvwa/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt
+chmod -R 777 /var/www/html/hackable/uploads/
+chmod -R 777 /var/www/html/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt
 echo "p@ssw0rd" > .password
 mysql --user=root --password="$(< .password)" -e "CREATE DATABASE dvwa;"
 mysql --user=root --password="$(< .password)" dvwa < /vagrant/dvwa-1.9-cnsec2017.sql
